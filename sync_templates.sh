4b8df3718cc020febf16f9ea59f0ebbd49da095e
Diego Castro <spinolacastro@gmail.com>
2016-02-23 09:18:27 -0300

    fix typo

--- sync_templates.sh
+++ sync_templates.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 git pull 
 
 for i in imagestreams/* templates/*; do
   oc replace -f $i -n openshift
-  if [ $? -ne 0]; then
+  if [ "$?" -ne 0 ]; then
     oc create -f $i -n openshift
   fi
 done
